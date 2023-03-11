039580402983e7a4152c05418782389ca776cd72
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2023-03-11 22:07:37 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,10 @@ Route::post('/', function(Request $request){
                     $audio = explode(",",  $audio)[1]; 
                     $audio = base64_decode($audio);
                     // Storage::put('audio.wav', $audio);
-                    $dir    = public_path().'/';
-                    $files = scandir($dir);
-                    dd($files); exit;
-                    file_put_contents('vn/audio.wav', $audio);
+//                     $dir    = public_path().'/';
+//                     $files = scandir($dir);
+//                     dd($files); exit;
+                    file_put_contents(public_path().'/audio.wav', $audio);
                     // file_put_contents('audio.wav', base64_decode($audio));
                     // ;fopen('audio.wav', 'r')
                     // dd('audio.wav');
@@ -45,7 +45,7 @@ Route::post('/', function(Request $request){
                     //echo 'yes'; return redirect('/'); exit;
                     $response = OpenAI::audio()->transcribe([
                         'model' => 'whisper-1',
-                        'file' => fopen('vn/audio.wav', 'r'),
+                        'file' => fopen('audio.wav', 'r'),
                         'response_format' => 'verbose_json',
                     ]); //dd($response); exit;
                     $nmsg = $response->segments[0]->text;
